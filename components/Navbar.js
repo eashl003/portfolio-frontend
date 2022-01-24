86d3b42906c7ea06d9b5734d0c4acfc5d971ba72
Lizzy <62777652+eashl003@users.noreply.github.com>
2022-01-24 06:01:10 -0500

    updated 5

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,7 +4,7 @@ import NextImage from "./Image"
 const Navbar = () => {
   return (
     <div className="flex justify-between ml-6 mr-6 mt-4">
-      <Link href="/">`elisabeth`</Link>
+      <Link href="/">elisabeth</Link>
       <button className="snipcart-checkout flex items-center">
         <NextImage height="150" width="150" src="/cart.svg" alt="Cart" />
         <span className="snipcart-total-price ml-3 font-semibold text-sm text-indigo-500"></span>
