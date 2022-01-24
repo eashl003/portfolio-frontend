8e5fdab5d9a2b838c21df38c7e15450de149343b
Lizzy <62777652+eashl003@users.noreply.github.com>
2022-01-24 16:36:34 -0500

    update nav

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,18 +1,14 @@
 import React from "react";
+import Link from "next/link"
 
 export default function Navbar({ fixed }) {
-  const [navbarOpen, setNavbarOpen] = React.useState(false);
+  const [navbarOpen, setNavbarOpen] = React.useState(false)
   return (
     <>
       <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 bg-emerald-500 mb-3">
         <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
           <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
-            <a
-              className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white"
-              href="#pablo"
-            >
-              emerald Tailwind Starter Kit
-            </a>
+            <Link className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white" href="/blog">Elisabeth</Link>
             <button
               className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
               type="button"
@@ -58,6 +54,6 @@ export default function Navbar({ fixed }) {
         </div>
       </nav>
     </>
-  );
+  )
 }
 
