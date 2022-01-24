66e80f696b0d42e7340192e3bbfde1f0b293ab8a
Lizzy <62777652+eashl003@users.noreply.github.com>
2022-01-24 17:31:31 -0500

    done

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,41 +1,33 @@
 import Link from "next/link"
-import { Navbar, Dropdown, NavDropdown, NavItem, Nav } from "react-bootstrap"
 
 const Header = () => {
   return (
-    <Navbar inverse collapseOnSelect>
-      <Navbar.Header>
-        <Navbar.Brand>
-          <Link href="#brand">React-Bootstrap</Link>
-        </Navbar.Brand>
-        <Navbar.Toggle />
-      </Navbar.Header>
-      <Navbar.Collapse>
-        <Nav>
-          <NavItem eventKey={1} href="#">
-            Link
-          </NavItem>
-          <NavItem eventKey={2} href="#">
-            Link
-          </NavItem>
-          <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-            <Dropdown eventKey={3.1}>Action</Dropdown>
-            <Dropdown eventKey={3.2}>Another action</Dropdown>
-            <Dropdown eventKey={3.3}>Something else here</Dropdown>
-            <Dropdown divider />
-            <Dropdown eventKey={3.3}>Separated link</Dropdown>
-          </NavDropdown>
-        </Nav>
-        <Nav pullRight>
-          <NavItem eventKey={1} href="#">
-            Link Right
-          </NavItem>
-          <NavItem eventKey={2} href="#">
-            Link Right
-          </NavItem>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
+    <nav class="flex items-center justify-between flex-wrap bg-teal-500 p-6">
+      <div class="flex items-center flex-shrink-0 text-white mr-6">
+        <span class="font-semibold text-xl tracking-tight">Test</span>
+      </div>
+      <div class="block lg:hidden">
+        <button class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
+          <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+        </button>
+      </div>
+      <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+        <div class="text-sm lg:flex-grow">
+          <Link href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+            Docs
+          </Link>
+          <Link href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+            Examples
+          </Link>
+          <Link href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white">
+            Blog
+          </Link>
+        </div>
+        <div>
+          <Link href="#" class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">Download</Link>
+        </div>
+      </div>
+    </nav>
   )
 }
 
