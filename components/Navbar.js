697a4a34b2911078e6312cfc61661ed87218a463
Lizzy <62777652+eashl003@users.noreply.github.com>
2022-01-24 16:54:51 -0500

    update

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,7 +2,7 @@ import Link from "next/link"
 import NextImage from "./Image"
 
 const Navbar = () => {
-  const [navbarOpen, setNavbarOpen] = React.useState(false)
+   
   return (
     <div className="flex justify-between ml-6 mr-6 mt-4">
       <Link href="/blog">elisabeth</Link>
