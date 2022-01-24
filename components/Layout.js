14bf9c1297731dc9d44ff3172b1c8bd5906bc05a
Lizzy <62777652+eashl003@users.noreply.github.com>
2022-01-24 17:28:17 -0500

    test

--- components/Layout.js
+++ components/Layout.js
@@ -1,12 +1,12 @@
 import CategoryButtons from "./CategoryButtons"
 import Footer from "./Footer"
-import Navbar from "./Navbar"
+import Header from "./Header"
 
 const Layout = ({ children, categories }) => {
   return (
     <div className="flex justify-center bg-gray-200">
       <div className="max-w-screen-lg flex flex-col min-h-screen w-full">
-        <Navbar />
+        <Header />
         <CategoryButtons categories={categories} />
         <div className="flex-grow">{children}</div>
         <Footer />
