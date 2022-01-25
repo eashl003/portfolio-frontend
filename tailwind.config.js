2ac0f80f8f2645197adc00886f3aa56e09f7c075
Lizzy <62777652+eashl003@users.noreply.github.com>
2022-01-25 06:14:34 -0500

    updated config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,18 +2,4 @@ const { colors } = require(`tailwindcss/defaultTheme`)
 
 module.exports = {
   purge: ["./components/**/*.js", "./pages/**/*.js", "./components/Navbar.js"],
-  theme: {
-    extend: {
-      colors: {
-        primary: colors.indigo,
-      },
-      container: {
-        center: true,
-        padding: {
-          default: "1rem",
-          md: "2rem",
-        },
-      },
-    },
-  },
 }
