bd4e3a3f1597801b842df32e0bf8d38a9230542e
Lizzy <62777652+eashl003@users.noreply.github.com>
2022-01-26 15:29:21 -0500

    update nav

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,24 +1,142 @@
+import React, { useState } from "react"
 import Link from "next/link"
-import NextImage from "./Image"
+import { Transition } from "@headlessui/react"
 
-const Navbar = () => {
+function Navbar() {
+  const [isOpen, setIsOpen] = useState(false)
   return (
-    <div className="flex justify-between ml-6 mr-6 mt-4">
-      <Link href="/">
-        <a>
-          <NextImage
-            src="/strapi.png"
-            alt="home"
-            className="logo"
-            height="44"
-            width="150"
-          />
-        </a>
-      </Link>
-      <button className="snipcart-checkout flex items-center">
-        <NextImage height="150" width="150" src="/cart.svg" alt="Cart" />
-        <span className="snipcart-total-price ml-3 font-semibold text-sm text-indigo-500"></span>
-      </button>
+    <div>
+      <nav className="bg-teal-800 text-white">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="flex items-center justify-between h-16">
+            <div className="flex items-center">
+              <div className="flex-shrink-0">elisabeth</div>
+              <div className="hidden md:block">
+                <div className="hover:bg-gray-700 ml-10 flex items-baseline space-x-4">
+                  <Link
+                    href="/blog"
+                    className="hover:bg-gray-700 text-white px-3 py-2 rounded-md text-sm font-medium"
+                  >
+                    Blog
+                  </Link>
+                  <Link
+                    href="#"
+                    className="hover:bg-gray-700 text-white px-3 py-2 rounded-md text-sm font-medium"
+                  >
+                    Shop
+                  </Link>
+                  <Link
+                    href="#"
+                    className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                  >
+                    Projects
+                  </Link>
+                  <Link
+                    href="#"
+                    className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                  >
+                    About
+                  </Link>
+                </div>
+              </div>
+            </div>
+            <div className="-mr-2 flex md:hidden">
+              <button
+                onClick={() => setIsOpen(!isOpen)}
+                type="button"
+                className="bg-gray-900 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+                aria-controls="mobile-menu"
+                aria-expanded="false"
+              >
+                <span className="sr-only">Open main menu</span>
+                {!isOpen ? (
+                  <svg
+                    className="block h-6 w-6"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M4 6h16M4 12h16M4 18h16"
+                    />
+                  </svg>
+                ) : (
+                  <svg
+                    className="block h-6 w-6"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
+                )}
+              </button>
+            </div>
+          </div>
+        </div>
+        <Transition
+          show={isOpen}
+          enter="transition ease-out duration-100 transform"
+          enterFrom="opacity-0 scale-95"
+          enterTo="opacity-100 scale-100"
+          leave="transition ease-in duration-75 transform"
+          leaveFrom="opacity-100 scale-100"
+          leaveTo="opacity-0 scale-95"
+        >
+          {(ref) => (
+            <div className="md:hidden" id="mobile-menu">
+              <div ref={ref} className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+                <ul>
+                  <li>
+                    <Link
+                      href="#"
+                      className="hover:bg-gray-700 text-white block px-3 py-2 rounded-md text-base font-medium"
+                    >
+                      Blog
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      href="#"
+                      className="hover:bg-gray-700 text-white block px-3 py-2 rounded-md text-base font-medium"
+                    >
+                      Shop
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      href="#"
+                      className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                    >
+                      Projects
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      href="#"
+                      className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                    >
+                      About
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          )}
+        </Transition>
+      </nav>
     </div>
   )
 }
